b0882f14074ec8b532651e763e793eb2592f2382
Tom Strother <tom_strother@yahoo.com>
2020-07-21 15:33:51 -0500

    save point

--- attractions/petting_zoo.py
+++ attractions/petting_zoo.py
@@ -6,8 +6,13 @@ class PettingZoo(Attraction):
     def __init__(self, name, description):
         super().__init__(name, description)
 
-    # def add_animal(self, animals):
-    #     self.animals.extend(animals)
+    def add_animal(self, animals):
+        try:
+            if animals.walk_speed > -1:
+                self.animals.append(animals)
+        except AttributeError as ex:
+            print(
+                f'{animal} doesn\'t like to be petted, so please do not put it in the {self.name} attraction.')
 
     @property
     def last_critter_added(self):
